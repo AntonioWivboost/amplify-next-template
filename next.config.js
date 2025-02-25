4f53bb5a77deb96f845e47430d30691b8522592b
ija54312 <ija54312@gmail.com>
2025-02-24 18:32:51 -0600

    add sass,lint,prettier

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
+const { join } = require("path");
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    sassOptions: {
+        includePaths: [join(__dirname, "src/sass")],
+      },
+}
 
 module.exports = nextConfig
